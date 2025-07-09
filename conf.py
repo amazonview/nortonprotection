109fa4797a4d0e3493f80bb8f97d8b7cb975c23d
amazonview <chetijasimran@gmail.com>
2025-07-09 13:32:28 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'Disneyplus TV'
+project = 'Norton Setup'
 
 
 # The full version, including alpha/beta/rc tags
